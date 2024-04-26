8221cf2e6686e8b76a4b75c785936b205003aab3
luqeta <lucasmicrosoft@gmail.com>
2024-04-26 10:29:20 -0300

    Média Alunos C#

---
+++ WindowsFormsAppMedia/media/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace media
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lblNota3 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.NOME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOTA1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOTA2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOTA3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MEDIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(22, 50);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(574, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(639, 50);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(189, 20);
+            this.txtNota1.TabIndex = 1;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(873, 50);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(180, 20);
+            this.txtNota2.TabIndex = 2;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(1092, 50);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(180, 20);
+            this.txtNota3.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(1307, 50);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(118, 20);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Adicionar";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(19, 34);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 5;
+            this.lblNome.Text = "Nome";
+            this.lblNome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Location = new System.Drawing.Point(636, 34);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(39, 13);
+            this.lblNota1.TabIndex = 6;
+            this.lblNota1.Text = "Nota 1";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Location = new System.Drawing.Point(870, 34);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(39, 13);
+            this.lblNota2.TabIndex = 7;
+            this.lblNota2.Text = "Nota 2";
+            // 
+            // lblNota3
+            // 
+            this.lblNota3.AutoSize = true;
+            this.lblNota3.Location = new System.Drawing.Point(1089, 34);
+            this.lblNota3.Name = "lblNota3";
+            this.lblNota3.Size = new System.Drawing.Size(39, 13);
+            this.lblNota3.TabIndex = 8;
+            this.lblNota3.Text = "Nota 3";
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NOME,
+            this.NOTA1,
+            this.NOTA2,
+            this.NOTA3,
+            this.MEDIA});
+            this.dgv.Location = new System.Drawing.Point(22, 105);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.Size = new System.Drawing.Size(1402, 671);
+            this.dgv.TabIndex = 9;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // NOME
+            // 
+            this.NOME.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NOME.HeaderText = "Nome";
+            this.NOME.Name = "NOME";
+            // 
+            // NOTA1
+            // 
+            this.NOTA1.HeaderText = "Nota 1";
+            this.NOTA1.Name = "NOTA1";
+            // 
+            // NOTA2
+            // 
+            this.NOTA2.HeaderText = "Nota 2";
+            this.NOTA2.Name = "NOTA2";
+            // 
+            // NOTA3
+            // 
+            this.NOTA3.HeaderText = "Nota 3";
+            this.NOTA3.Name = "NOTA3";
+            // 
+            // MEDIA
+            // 
+            this.MEDIA.HeaderText = "Média";
+            this.MEDIA.Name = "MEDIA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1465, 827);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.lblNota3);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Sistema de Notas e Médias";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lblNota3;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOTA1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOTA2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOTA3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MEDIA;
+    }
+}
+
---
+++ WindowsFormsAppMedia/media/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace media
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Botão para inserir as notas:
+
+            //Calculo da Média
+            Decimal nota1 = Convert.ToDecimal(txtNota1.Text);
+            Decimal nota2 = Convert.ToDecimal(txtNota2.Text);
+            Decimal nota3 = Convert.ToDecimal(txtNota3.Text);
+            Decimal media = (nota1 + nota2 + nota3) / 3;
+            media = decimal.Round(media, 2);
+
+            //Add no DataGridView
+
+            dgv.Rows.Add();
+            dgv.Rows[dgv.Rows.Count - 1].Cells["NOME"].Value = txtNome.Text;
+            dgv.Rows[dgv.Rows.Count - 1].Cells["NOTAS1"].Value = nota1;
+            dgv.Rows[dgv.Rows.Count - 1].Cells["NOTAS2"].Value = nota2;
+            dgv.Rows[dgv.Rows.Count - 1].Cells["NOTAS3"].Value = nota3;
+            dgv.Rows[dgv.Rows.Count - 1].Cells["MEDIA"].Value = media;
+
+        }
+    }
+}
